a250f61f3e98efc1964bfe36968e69d118bc67fe
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-02-14 23:39:31 -0300

    Revert "Fix malloc() and free() prototype"
    This reverts commit 6b2467e3ebb191e1026de849a013b42d944e3f2a.

--- include/stdlib.h
+++ include/stdlib.h
@@ -1,8 +1,8 @@
 #ifndef _STDLIB_INCLUDE_
 #define _STDLIB_INCLUDE_
 
-VOID *malloc(UINTN);
-VOID free(VOID *);
+VOID *bmalloc(UINTN);
+VOID bfree(VOID *);
 VOID uitoh(CHAR16 *, UINT64);
 VOID itos(CHAR16 *, INTN);
 
