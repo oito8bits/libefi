192a7bef96a0c8753f2442ec7b8746de18739ae6
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-02-14 23:40:50 -0300

    Revert "Organize code"
    This reverts commit 90c95ca69c9deb77d5e86d7fe68d6ee293652e2b.

--- include/stdio.h
+++
@@ -1,8 +0,0 @@
-#ifndef _STDIO_INCLUDE_
-#define _STDIO_INCLUDE_
-
-VOID puts(const CHAR16 *);
-VOID putchar(CHAR16);
-INTN printf(const CHAR16 *, ...);
-
-#endif
--- include/stdlib.h
+++
@@ -1,9 +0,0 @@
-#ifndef _STDLIB_INCLUDE_
-#define _STDLIB_INCLUDE_
-
-VOID *bmalloc(UINTN);
-VOID bfree(VOID *);
-VOID uitoh(CHAR16 *, UINT64);
-VOID itos(CHAR16 *, INTN);
-
-#endif
