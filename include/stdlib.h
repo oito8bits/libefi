6b2467e3ebb191e1026de849a013b42d944e3f2a
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-02-14 23:18:41 -0300

    Fix malloc() and free() prototype

--- include/stdlib.h
+++ include/stdlib.h
@@ -1,8 +1,8 @@
 #ifndef _STDLIB_INCLUDE_
 #define _STDLIB_INCLUDE_
 
-VOID *bmalloc(UINTN);
-VOID bfree(VOID *);
+VOID *malloc(UINTN);
+VOID free(VOID *);
 VOID uitoh(CHAR16 *, UINT64);
 VOID itos(CHAR16 *, INTN);
 
