3002394ec23b82c26fa67edfcdc0a46363abcdcd
oito8bits <williandossantosdeoliveira287@gmail.com>
2025-07-11 22:47:52 -0300

    Add EFI_FILE_SYSTEM_INFO

--- include/efiprot.h
+++ include/efiprot.h
@@ -120,6 +120,36 @@ typedef struct _EFI_BLOCK_IO_PROTOCOL
   EFI_BLOCK_WRITE     WriteBlocks;
   EFI_BLOCK_FLUSH     FLushBlocks;
 } EFI_BLOCK_IO_PROTOCOL;
+
+/*
+ *=============================
+ * Efi File Info
+ *=============================
+ */
+
+#define EFI_FILE_INFO_ID \
+  {0x09576e92,0x6d3f,0x11d2,\
+  {0x8e39,0x00,0xa0,0xc9,0x69,0x72,0x3b}}
+
+#define EFI_FILE_READ_ONLY 0x0000000000000001
+#define EFI_FILE_HIDDEN 0x0000000000000002
+#define EFI_FILE_SYSTEM 0x0000000000000004
+#define EFI_FILE_RESERVED 0x0000000000000008
+#define EFI_FILE_DIRECTORY 0x0000000000000010
+#define EFI_FILE_ARCHIVE 0x0000000000000020
+#define EFI_FILE_VALID_ATTR 0x0000000000000037
+
+typedef struct {
+ UINT64 Size;
+ UINT64 FileSize;
+ UINT64 PhysicalSize;
+ EFI_TIME CreateTime;
+ EFI_TIME LastAccessTime;
+ EFI_TIME ModificationTime;
+ UINT64 Attribute;
+ CHAR16 FileName[];
+} EFI_FILE_INFO;
+
 /*
  *=============================
  * Efi File Protocol.
